34e2615d8db46e38d22f60db02888b6b7db5dd0f
JordyDutch <jordy@sigmatic.io>
2025-01-16 22:49:27 +0100

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,23 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Luksoverse',
   description: 'Luksoverse - News, Events & Discussions',
+  icons: {
+    icon: [
+      {
+        url: '/favicon.ico',
+        sizes: 'any',
+      },
+      {
+        url: '/icon.png',
+        type: 'image/png',
+        sizes: '32x32',
+      },
+    ],
+    apple: {
+      url: '/apple-icon.png',
+      sizes: '180x180',
+    },
+  },
 }
 
 export default function RootLayout({
@@ -17,6 +34,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.png" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
